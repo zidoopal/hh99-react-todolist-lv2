590e7895ea28eecfdbfc8935b4f1b7cd669fc334
zidoopal <yuzzzin@gmail.com>
2023-09-01 17:29:28 +0900

    input 입력 값 없을 시 등록안되게 수정, CSS 수정

--- src/components/TodoInput.jsx
+++ src/components/TodoInput.jsx
@@ -1,44 +1,14 @@
-import { useState } from "react";
-import { useDispatch } from "react-redux";
-import { addTodo } from "../redux/modules/todo";
-import { styled } from "styled-components";
-
-const StInputBox = styled.div`
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  justify-content: center;
-
-  border: 3px solid #007bff;
-  border-radius: 8px;
-  margin: 20px auto;
-
-  font-size: 30px;
-  height: 100px;
-  margin-bottom: 10px;
-  padding: 0px 20px 0px 20px;
-  gap: 10px;
-`;
-
-const StInput = styled.input`
-  max-width: 75%;
-  font-size: 25px;
-  font-weight: 500;
-  margin-bottom: 0px;
-  border-radius: 13px;
-  padding: 5px;
-`;
-
-const StAddButton = styled.button`
-  margin-left: auto;
-  font-size: 20px;
-  border-radius: 10px;
-`;
+import { useState, useRef } from 'react';
+import { useDispatch } from 'react-redux';
+import { addTodo } from '../redux/modules/todo';
+import { StInput, StAddButton, StInputBox } from '../styles/styles';
 
 function TodoInput() {
-  const [title, setTitle] = useState("");
-  const [contents, setContents] = useState("");
+  const [title, setTitle] = useState('');
+  const [contents, setContents] = useState('');
   const dispatch = useDispatch();
+  const titleInput = useRef();
+  const contentsInput = useRef();
 
   const titleOnChangeHandler = (event) => {
     setTitle(event.target.value);
@@ -48,21 +18,41 @@ function TodoInput() {
     setContents(event.target.value);
   };
 
-  const addTodoHandler = () => {
+  // input에 입력값 없을 시  alert 대신 focus 효과로 대신해보기
+  const addTodoHandler = (e) => {
+    e.preventDefault();
+
+    if (title.length < 1 || contents.length < 1) {
+      if (title.length < 1) {
+        titleInput.current.focus();
+      } else {
+        contentsInput.current.focus();
+      }
+      return;
+    }
+
     dispatch(addTodo({ title, contents }));
-    setTitle("");
-    setContents("");
+
+    // 저장 후 작성 폼 데이터 초기값으로
+    setTitle('');
+    setContents('');
   };
 
   return (
     <StInputBox>
       제목
-      <StInput type="text" value={title} onChange={titleOnChangeHandler} />
+      <StInput
+        type="text"
+        value={title}
+        onChange={titleOnChangeHandler}
+        ref={titleInput}
+      />
       내용
       <StInput
         type="text"
         value={contents}
         onChange={contentsOnChangeHandler}
+        ref={contentsInput}
       />
       <StAddButton onClick={addTodoHandler}>추가하기</StAddButton>
     </StInputBox>
